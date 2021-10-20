6b7c5f1097d7e3d485ef81edcc9773963f593886
Alexander Goshadze <jonsnow483@gmail.com>
2021-10-20 19:37:31 +0300

    Update ijl.config.js

--- ijl.config.js
+++ ijl.config.js
@@ -1,9 +1,4 @@
-const pkg = require('./package');
-
-const versions = process.env.VERSION ? 'main': pkg.version;
-
 module.exports = {
-  apiPath: 'stubs/api',
   webpackConfig: {
     output: {
       publicPath: `/static/${pkg.name}/${versions}/`,
@@ -11,16 +6,8 @@ module.exports = {
   },
   features: {
     'innoreact': {
-      registration: true,
-      map: true,
       main: true,
-      message: true,
     },
-  },
-  config: {
-    'yandex.map.key': '92c61db5-c1dd-4111-ba56-491f6a0a4975',
-    'yandex.url.map.key': 'https://api-maps.yandex.ru/2.1/?apikey',
-    'innoreact.api': '/api',
   },
   module: {
     rules: [
@@ -40,9 +27,5 @@ module.exports = {
   },
   navigations: {
     'innoreact': '/innoreact',
-    'link.innoreact.main': '/innoreact/main',
-    'link.innoreact.map': '/innoreact/map',
-    'link.innoreact.registration': '/innoreact/registration',
-    'link.innoreact.price': '/innoreact/price',
   },
 };

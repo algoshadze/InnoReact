a4a7a4c43c9c09f1b60771a08f0b55f9d4114940
Alexander Goshadze <jonsnow483@gmail.com>
2021-10-20 19:36:35 +0300

    Delete stubs/api directory
    Remove a excess directory

--- stubs/api/auth/index.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line new-cap
-const router = require('express').Router();
-
-router.use(require('./login'));
-router.use(require('./registration'));
-
-module.exports = router;
--- stubs/api/index.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line new-cap
-const router = require('express').Router();
-
-router.use(require('./carousel'));
-router.use(require('./map'));
-router.use('/auth', require('./auth'));
-router.use(require('./prices'));
-
-module.exports = router;
